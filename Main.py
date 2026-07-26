37ea94bee013d0d977dbc36e76aa4fb622d86255
GreenMan <271954502+GreenMan6300@users.noreply.github.com>
2026-07-26 16:07:18 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,29 +4,36 @@ from tkinter import filedialog
 
 print("Python IDE (Simple Edition)")
 print("Created by GreenMan6300")
-input("Press enter to start")
-print("Starting..")
+print("Update log:")
+print("1: new folder called Images")
+print("2: removed press enter to start")
+print("3: new icon and a new Logo")
+print("4: text that has the name changed to the Logo")
+print("5: Background and button and text is Blue")
+print("6: save and quit isnt a sandwich anymore")
 
 #Required to create window and set dimension and set the title of the Window
 window = Tk()
-window.geometry("460x460")
+window.geometry("500x500")
 window.title("Python IDE (simple edition)")
 #Changes the window appearance and icon
-icon = PhotoImage(file='icon.png')
+icon = PhotoImage(file='Images/icon.png')
+logo = PhotoImage(file='Images/logo.png')
 window.iconphoto(True, icon)
-window.config(background="#F0F0F0")
+window.config(background="#A6C7D6")
 
 #The name of the Program Text!
-label = Label(window, text="Python IDE (simple edition)")
-label.config(font=("Arial", 20))
-label.pack()
+labellogo = Label(window, image=logo)
+labellogo.config(background="#A6C7D6")
+labellogo.pack()
 
 #Just to tell the user that there is no load or run
 label = Label(window, text="Does not include it's own way to Run code or load .py")
 label.config(font=("Arial", 10))
+label.config(background="#A6C7D6")
 label.pack()
 
-#The so annoying system i had to deal whit
+#The so annoying system i had to deal whit for save button
 def saveFile():
     file = filedialog.asksaveasfile(defaultextension=".py",
                                     filetypes=[
@@ -34,26 +41,38 @@ def saveFile():
                                     ])
     filetext = str(text.get(1.0, END))
     file.write(filetext)
+    file.config(background="#A6C7D6")
     file.close()
 
 #Save Button
 button = Button(text="Save",command=saveFile)
 button.config(font=("Arial", 12))
-button.config(bg="white", fg="black")
-button.pack()
+button.config(bg="#CBEDFF", fg="#2F363B")
+button.config(activebackground="#2F363B")
+button.config(activeforeground="#CBEDFF")
+button.place(x=200, y=89)
 
 #Quit Button
 def exit():
     window.destroy()
-    print("Stopping..")
+    print("Session ended")
 
 button = Button(text="Quit",command=exit)
 button.config(font=("Arial", 12))
-button.config(bg="white", fg="black")
-button.pack()
+button.config(bg="#CBEDFF", fg="#2F363B")
+button.config(activebackground="#2F363B")
+button.config(activeforeground="#CBEDFF")
+button.place(x=250, y=89)
+
+#Just to tell the user that there is no load or run
+label = Label(window, text="")
+label.config(font=("Arial", 18))
+label.config(background="#A6C7D6")
+label.pack()
 
 #Text Editor
 text = Text(window)
+text.config(background="#A3DCFF")
 text.pack()
 
 #Creates the Window
